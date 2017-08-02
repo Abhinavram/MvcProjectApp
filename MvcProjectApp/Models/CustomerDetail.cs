7e31bf84159a55d172b37341fc9dd1965a513e04
AbhinavRam <avedantamkodanda@unomaha.edu>
2017-08-01 23:49:56 -0500

    Initial Commit along with Service Reference

---
+++ MvcProjectApp/Models/CustomerDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcProjectApp.Models
+{
+    public class CustomerDetail
+    {
+        public int CustomerId { get; set; }
+
+        public string CustorName { get; set; }
+
+        public string Address { get; set; }
+    }
+}
